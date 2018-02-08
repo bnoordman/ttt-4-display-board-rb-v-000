7dc817d44c891e01ec2abf41018d4f87482fc0e4
Brittany Young <mrsbrityoung@gmail.com>
2018-02-08 23:47:38 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -4,7 +4,7 @@ describe "#display_board in 'lib/display_board.rb" do
   context 'various game situations' do
     it 'prints a blank board when the board array is empty' do
       board = [" "," "," "," "," "," "," "," "," "]
-  binding.pry 
+      
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
 
